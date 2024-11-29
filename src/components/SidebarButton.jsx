e07ec13b76cee924e826ae98ece98ac8dc17cf59
Erick Araújo <araujoerick.dev@gmail.com>
2024-11-29 15:30:10 -0300

    :sparkles: feat: add svg icons to Sidebar component

--- src/components/SidebarButton.jsx
+++ src/components/SidebarButton.jsx
@@ -11,7 +11,7 @@ const SidebarButton = ({ children, variant }) => {
   return (
     <a
       href="#"
-      className={`rounded-lg px-6 py-3 text-left hover:bg-primary/10 ${getVariantClasses()}`}
+      className={`flex items-center gap-2 rounded-lg px-6 py-3 text-left hover:bg-primary/10 ${getVariantClasses()}`}
     >
       {children}
     </a>
