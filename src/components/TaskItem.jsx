0429b1dca430552ce1cb07ac6ce6a4d119ab374c
Erick Araújo <araujoerick.dev@gmail.com>
2024-12-01 18:30:41 -0300

    :sparkles: feat: ensure TaskItem changes depending on task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,12 +1,14 @@
-// import CheckIcon from "../assets/icons/check.svg?react";
+import CheckIcon from "../assets/icons/check.svg?react";
+import LoaderIcon from "../assets/icons/loader.svg?react";
+import DetailsIcon from "../assets/icons/details.svg?react";
 
 const TaskItem = ({ task }) => {
-  const getStatus = () => {
+  const getStatusClasses = () => {
     if (task.status === "done") {
-      return "bg-primary/10 text-primary";
+      return "bg-primary text-primary";
     }
     if (task.status === "in_progress") {
-      return "bg-[#FFAA04]/10 text-[#FFAA04]";
+      return "bg-[#FFAA04] text-[#FFAA04]";
     }
     if (task.status === "not_started") {
       return "bg-[#35383E]/10 text-[#35383E]";
@@ -15,10 +17,28 @@ const TaskItem = ({ task }) => {
 
   return (
     <div
-      className={`flex items-center gap-3 rounded-lg p-3 text-sm ${getStatus()}`}
+      className={`flex items-center justify-between gap-3 rounded-lg bg-opacity-10 p-3 text-sm ${getStatusClasses()}`}
     >
-      {/* <CheckIcon className="h-6 w-6 rounded-md bg-primary p-1" /> */}
-      {task.title}
+      <div className="flex items-center gap-2">
+        <label
+          className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg bg-opacity-100 ${getStatusClasses()}`}
+        >
+          <input
+            type="checkbox"
+            checked={task.status === "done"}
+            className="absolute h-full w-full cursor-pointer opacity-0"
+            onChange={() => {}}
+          />
+          {task.status === "done" && <CheckIcon />}
+          {task.status === "in_progress" && (
+            <LoaderIcon className="animate-[spin_2s_ease-in-out] text-white" />
+          )}
+        </label>
+        {task.title}
+      </div>
+      <a href="#" className="transition hover:opacity-75">
+        <DetailsIcon />
+      </a>
     </div>
   );
 };
