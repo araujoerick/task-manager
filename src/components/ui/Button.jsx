07c139dac934179e3b371e352151cecabaf93b39
Erick Araújo <araujoerick.dev@gmail.com>
2024-12-19 11:23:09 -0300

    ✨ feat: add initial TaskDetails page without logic

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -6,16 +6,17 @@ const Button = ({ children, className, color, size, ...rest }) => {
     base: "flex items-center justify-center gap-2 rounded-md font-semibold transition disabled:cursor-not-allowed disabled:opacity-50",
     variants: {
       color: {
-        primary: "bg-brand-primary text-white hover:bg-brand-primary/70",
+        primary: "bg-brand-primary text-brand-white hover:bg-brand-primary/70",
         secondary:
           "bg-brand-dark-blue/10 text-brand-dark-blue hover:bg-brand-dark-blue/15",
         ghost:
           "bg-transparent text-brand-dark-gray hover:bg-brand-dark-gray/15",
+        danger: "bg-brand-danger text-brand-white hover:bg-brand-danger/80",
       },
       size: {
         small: "px-3 py-1 text-xs",
-        medium: "px-6 py-2 text-sm",
-        large: "px-8 py-3 text-base",
+        medium: "px-4 py-2 text-sm",
+        large: "px-6 py-3 text-base",
       },
     },
     defaultVariants: {
@@ -34,7 +35,7 @@ const Button = ({ children, className, color, size, ...rest }) => {
 Button.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
-  color: PropTypes.oneOf(["primary", "secondary", "ghost"]),
+  color: PropTypes.oneOf(["primary", "secondary", "ghost", "danger"]),
   size: PropTypes.oneOf(["small", "medium", "large"]),
 };
 
--- src/pages/task-details.jsx
+++ src/pages/task-details.jsx
@@ -1,9 +1,20 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { ArrowLeftIcon, ChevronRightIcon, TrashIcon } from "../assets/icons";
+import Sidebar from "../components/Sidebar";
+import Button from "../components/ui/Button";
+import Input from "../components/ui/Input";
+import TimeSelect from "../components/ui/TimeSelect";
 
 const TaskDetailsPage = () => {
   const { taskId } = useParams();
   const [task, setTask] = useState();
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
 
   useEffect(() => {
     const fetchTask = async () => {
@@ -16,11 +27,66 @@ const TaskDetailsPage = () => {
   }, [taskId]);
 
   return (
-    <div>
-      <h1>Detalhes da Tarefa</h1>
-      <p>Titulo: {task?.title}</p>
-      <p>Descricão: {task?.description}</p>
-      <p>Horário: {task?.time}</p>
+    <div className="flex">
+      <Sidebar />
+      <section className="flex w-full flex-col gap-6 px-8 py-16">
+        <div>
+          <button
+            onClick={handleBackClick}
+            className="flex h-7 w-7 items-center justify-center rounded-full bg-brand-primary"
+          >
+            <ArrowLeftIcon />
+          </button>
+          <div className="mb-2 mt-3 flex items-center gap-2 text-brand-text-gray">
+            <span className="cursor-pointer text-xs" onClick={handleBackClick}>
+              Minhas Tarefas
+            </span>
+            <ChevronRightIcon />
+            <span className="text-xs font-semibold text-brand-primary">
+              {task?.title}
+            </span>
+          </div>
+          <div className="flex items-center justify-between gap-2">
+            <h1 className="text-xl font-semibold">{task?.title}</h1>
+            <Button color="danger">
+              <TrashIcon />
+              Deletar tarefa
+            </Button>
+          </div>
+        </div>
+
+        <div className="space-y-6 rounded-xl bg-brand-white p-6">
+          <div>
+            <Input id="title" label="Título" value={task?.title} />
+          </div>
+          <div>
+            <TimeSelect value={task?.time} />
+          </div>
+          <div>
+            <Input
+              id="description"
+              label="Descrição"
+              value={task?.description}
+            />
+          </div>
+
+          {/* <Input
+                id="title"
+                label="Título"
+                placeholder="Nome da tarefa"
+                error={titleError}
+                ref={titleRef}
+                disabled={isLoading}
+              /> */}
+        </div>
+
+        <div className="flex w-full justify-end gap-2">
+          <Button size="medium" color="secondary">
+            Cancelar
+          </Button>
+          <Button size="medium">Salvar</Button>
+        </div>
+      </section>
     </div>
   );
 };
