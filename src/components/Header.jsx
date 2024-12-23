a5bf4d4d6497ab6bfd3925c8e32ec88ce4d33bfc
Erick Araújo <araujoerick.dev@gmail.com>
2024-12-22 21:58:55 -0300

    ✨ feat: add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+import { AddIcon, TrashIcon } from "../assets/icons";
+import AddTaskDialog from "./AddTaskDialog";
+import Button from "./ui/Button";
+
+const Header = ({ subtitle, title }) => {
+  const [addTaskDialogIsOpen, setAddTaskDialogIsOpen] = useState(false);
+
+  return (
+    <div className="flex w-full justify-between">
+      <div>
+        <span className="text-xs font-semibold text-brand-primary">
+          {subtitle}
+        </span>
+        <h2 className="text-xl font-semibold">{title}</h2>
+      </div>
+      <div className="flex items-center gap-3 self-end">
+        <Button color="ghost">
+          Limpar tarefas
+          <TrashIcon />
+        </Button>
+        <Button onClick={() => setAddTaskDialogIsOpen(true)}>
+          Nova tarefa
+          <AddIcon />
+        </Button>
+
+        <AddTaskDialog
+          isOpen={addTaskDialogIsOpen}
+          handleClose={() => setAddTaskDialogIsOpen(false)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
