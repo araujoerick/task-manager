d669797c653187346bda7d9adf785edd4e0f2354
Erick Araújo <araujoerick.dev@gmail.com>
2024-12-24 19:26:10 -0300

    📱 add responsive breakpoints to DashboardCards

--- src/components/ui/DashboardCard.jsx
+++ src/components/ui/DashboardCard.jsx
@@ -1,6 +1,6 @@
 const DashboardCard = ({ icon, mainText, secondaryText }) => {
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-2 rounded-[10px] bg-white py-10 last:hidden xl:last:flex">
+    <div className="flex w-full flex-col items-center justify-center gap-2 rounded-[10px] bg-white py-10 first:hidden last:hidden md:first:flex xl:last:flex">
       <div className="flex items-center gap-2">
         <span className="text-brand-primary">{icon}</span>
         <p className="text-3xl font-semibold">{mainText}</p>
